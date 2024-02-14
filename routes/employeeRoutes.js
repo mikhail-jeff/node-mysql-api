5caac767a3de68530ecd6cb86d06adf1c2854a31
jeffmikhail <mikhailcruz78@gmail.com>
2024-02-14 20:09:15 +0800

    update employee endpoint

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -90,4 +90,54 @@ router.get("/:id", async (request, response) => {
 	}
 });
 
+// *** UPDATE employee endpoint
+router.put("/:id", async (request, response) => {
+	const { id } = request.params;
+	const { name, email, gender, mobile } = request.body;
+
+	try {
+		// Check if the employee exists
+		const employeeExists = await Employee.findOne({
+			where: { id },
+		});
+
+		if (!employeeExists) {
+			return response.status(404).json({
+				message: "Employee not found",
+			});
+		}
+
+		const updatedEmployee = await Employee.update(
+			{
+				name,
+				email,
+				gender,
+				mobile,
+			},
+			{ where: { id } }
+		);
+
+		// Check the number of rows affected to confirm if the update was successful
+		if (updatedEmployee[0] === 1) {
+			const updatedEmployeeData = await Employee.findOne({
+				where: { id },
+			});
+
+			return response.status(200).json({
+				message: "Employee updated successfully",
+				employee: updatedEmployeeData,
+			});
+		} else {
+			return response.status(500).json({
+				message: "Failed to update employee",
+			});
+		}
+	} catch (error) {
+		console.log(error);
+		response.status(500).json({
+			message: "Failed to execute query",
+		});
+	}
+});
+
 module.exports = router;
